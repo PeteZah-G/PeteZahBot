52a7a1bf9b1c07f7fe8b816a7103f8da1b8b9fb2
PeteZah <petezahiscool@gmail.com>
2025-08-04 16:36:03 -0400

    Update run_bot.sh

--- run_bot.sh
+++ run_bot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source venv/bin/activate
 while true; do
-    python3 haven_cloud.py >> bot.log 2>&1
+    python3 petezah_bot.py >> bot.log 2>&1
     echo "Bot crashed with exit code $?. Restarting in 5 seconds..."
     sleep 5
 done
